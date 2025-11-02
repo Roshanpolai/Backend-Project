7360ebc972600a2dd8858b8c36338886339a4bfb
Roshan polai <roshanpolai3@gmail.com>
2025-11-02 15:34:15 +0530

    util code discussion

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,29 @@
+const asyncHnadler = (requestHandler) => {
+    (req, res, next) => {
+        Promise,resolve(requestHandler(req, res, next))
+        .catch((err) => next(err))
+    }
+}
+
+
+
+export {asyncHnadler}
+
+
+
+
+
+
+
+
+// const asyncHnadler = (fn) => async(req, res, next) => {
+//     try{
+//         await fn(req, res, next)
+//     }
+//     catch(error){
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
